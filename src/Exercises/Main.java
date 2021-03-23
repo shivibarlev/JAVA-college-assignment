133ff03fbfd29dcf3b0f8ec39a21af123bcb2b70
Elyashiv Bar lev <shivi.barlev@gmail.com>
2021-03-23 22:56:06 +0200

    add to github

---
+++ src/Exercises/Main.java
@@ -0,0 +1,19 @@
+package Exercises;
+import java.util.Scanner;
+
+
+public class Main
+{
+    static Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        String[] arguments;
+
+        System.out.println("Please enter all arguments: <command> <url> <list of arguments>\n" + "<command>: one character (mandatory)\n" +
+                    "<url>: a string (mandatory)\n" +
+                    "<list of arguments>: a list of string separated by spaces, possibly empty\n");
+
+        arguments = reader.nextLine().split(" ");
+    }
+}
